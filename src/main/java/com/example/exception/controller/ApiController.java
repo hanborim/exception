9effa9577c1a2b3dff6a386aea855cc75a11d5bb
borimhan <qhfla299653@gmail.com>
2022-08-06 16:06:03 +0900

    Initial commit

---
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -0,0 +1,33 @@
+package com.example.exception.controller;
+
+
+import com.example.exception.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/user")
+public class ApiController {
+
+    @GetMapping()
+    public User get(@RequestParam(required = false) String name , @RequestParam(required = false) Integer age)
+    {
+        //(required = false) 리퀘스트 파라미터가 없어도 동작할수 있도록함
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+
+        int a = 10+age;
+
+        return user;
+    }
+
+    @PostMapping()
+    public User post(@Valid @RequestBody User user)
+    {
+
+        System.out.println(user);
+        return user;
+    }
+}
